ba601ecdea8d2ca43531e1bfd1f4be256a64ffab
revou-fundamental-course <beach.jm02@gmail.com>
2024-03-27 14:23:30 +0700

    first push

---
+++ script.js
@@ -0,0 +1,131 @@
+function printError(Id, Msg) {
+    document.getElementById(Id).innerHTML = Msg;
+}
+
+function validateForm() {
+
+    var name = document.Form.name.value;
+    var email = document.Form.email.value;
+    var mobile = document.Form.mobile.value;
+    var country = document.Form.country.value;
+    var gender = document.Form.gender.value;
+    
+
+    var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
+    
+
+    if(name == "") {
+        printError("nameErr", "Tolong masukkan Nama Anda");
+        var elem = document.getElementById("name");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(name) === false) {
+            printError("nameErr", "Masukkan Nama dengan benar");
+            var elem = document.getElementById("name");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+            var elem = document.getElementById("name");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");            
+        }
+    }
+    
+
+    if(email == "") {
+        printError("emailErr", "Masukkan alamat email anda");
+         var elem = document.getElementById("email");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Masukkan alamat email yang benar");
+            var elem = document.getElementById("email");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+             var elem = document.getElementById("email");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+
+        }
+    }
+    
+ 
+    if(mobile == "") {
+        printError("mobileErr", "Tolong masukkan nomor hp/wa");
+        var elem = document.getElementById("mobile");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+    } else {
+        //var regex = /^\d+$/;
+        var regex = /^(\+62|62|0)8[1-9][0-9]{6,9}$/;
+        if(regex.test(mobile) === false) {
+            printError("mobileErr", "Nomor HP/WA tidak benar");
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-2");
+            elem.classList.remove("input-1");
+        } else{
+            printError("mobileErr", "");
+            mobileErr = false;
+            var elem = document.getElementById("mobile");
+            elem.classList.add("input-1");
+            elem.classList.remove("input-2");
+        }
+    }
+    
+
+    if(country == "Select") {
+        printError("countryErr", "Pilih paket yang di inginkan");
+        var elem = document.getElementById("country");
+            elem.classList.add("input-4");
+            elem.classList.remove("input-3");
+    } else {
+        printError("countryErr", "");
+        countryErr = false;
+        var elem = document.getElementById("country");
+            elem.classList.add("input-3");
+            elem.classList.remove("input-4");
+    }
+    
+
+    if(gender == "") {
+        printError("genderErr", "Silahkan pilih jenis kelamin anda");
+        var elem = document.getElementById("gender");
+            elem.classList.add("input-4");
+            elem.classList.remove("input-3");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+        var elem = document.getElementById("gender");
+            elem.classList.add("input-3");
+            elem.classList.remove("input-4");
+    }
+    
+    
+    if((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
+       return false;
+    } 
+}
+
+// koding js slide img
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+};
